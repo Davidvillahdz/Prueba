d9c8d0c6142d4d6c7bb7a46bcd7766dd442a668d
Davidvillahdz <d01072004@gmail.com>
2025-12-01 20:16:29 -0500

    Deploy a GitHub Pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginPage } from "./pages/login-page/login-page";
+import { HomePage } from "./pages/home-page/home-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginPage],
+  imports: [RouterOutlet, LoginPage, HomePage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
